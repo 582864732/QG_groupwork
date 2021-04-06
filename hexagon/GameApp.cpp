d644bdb4be7992b7d1b69f6785e2bd66460381ee
582864732 <582864732@qq.com>
2021-04-06 15:50:35 +0800

    三角型

--- hexagon/GameApp.cpp
+++ hexagon/GameApp.cpp
@@ -75,26 +75,83 @@ bool GameApp::InitEffect()
 
 bool GameApp::InitResource()
 {
-	 //设置三角形顶点
-	VertexPosColor vertices[] =
-	{
-		{ XMFLOAT3(0.0f, 0.5f, 0.5f), XMFLOAT4(0.0f, 1.0f, 0.0f, 1.0f) },
-		{ XMFLOAT3(0.5f, -0.5f, 0.5f), XMFLOAT4(0.0f, 0.0f, 1.0f, 1.0f) },
-		{ XMFLOAT3(-0.5f, -0.5f, 0.5f), XMFLOAT4(1.0f, 0.0f, 0.0f, 1.0f) }
-	};
+	// 设置三角形顶点
+	//VertexPosColor vertices[] =
+	//{
+	//	{ XMFLOAT3(0.0f, 0.5f, 0.5f), XMFLOAT4(0.0f, 1.0f, 0.0f, 1.0f) },
+	//	{ XMFLOAT3(0.5f, -0.5f, 0.5f), XMFLOAT4(0.0f, 0.0f, 1.0f, 1.0f) },
+	//	{ XMFLOAT3(-0.5f, -0.5f, 0.5f), XMFLOAT4(1.0f, 0.0f, 0.0f, 1.0f) }
+	//};
 
+	VertexPosColor vertices1[] =
+	{
+		{XMFLOAT3(0.25f,0.5f,0.5f),XMFLOAT4(1.0f,1.0f,1.0f,1.0f)},
+		{XMFLOAT3(0.5f,0.0f,0.5f),XMFLOAT4(1.0f,1.0f,1.0f,1.0f)},
+		{XMFLOAT3(0.25f,-0.5f,0.5f),XMFLOAT4(1.0f,1.0f,1.0f,1.0f)},
+	};
+	VertexPosColor vertices2[] =
+	{
+		{XMFLOAT3(-0.25f,0.5f,0.5f),XMFLOAT4(1.0f,1.0f,1.0f,1.0f)},
+		{XMFLOAT3(0.25f,0.5f,0.5f),XMFLOAT4(1.0f,1.0f,1.0f,1.0f)},
+		{XMFLOAT3(0.5f,0.0f,0.5f),XMFLOAT4(1.0f,1.0f,1.0f,1.0f)},
+	};
+	VertexPosColor vertices3[] =
+	{
+		{XMFLOAT3(-0.25f,0.5f,0.5f),XMFLOAT4(1.0f,1.0f,1.0f,1.0f)},
+		{XMFLOAT3(0.25f,-0.5f,0.5f),XMFLOAT4(1.0f,1.0f,1.0f,1.0f)},
+		{XMFLOAT3(-0.25f,-0.5f,0.5f),XMFLOAT4(1.0f,1.0f,1.0f,1.0f)},
+	};
+	VertexPosColor vertices4[] =
+	{
+		{XMFLOAT3(-0.25f,-0.5f,0.5f),XMFLOAT4(1.0f,1.0f,1.0f,1.0f)},
+		{XMFLOAT3(-0.5f,0.0f,0.5f),XMFLOAT4(1.0f,1.0f,1.0f,1.0f)},
+		{XMFLOAT3(-0.25f,0.5f,0.5f),XMFLOAT4(1.0f,1.0f,1.0f,1.0f)},
+	};
 	// 设置顶点缓冲区描述
 	D3D11_BUFFER_DESC vbd;
 	ZeroMemory(&vbd, sizeof(vbd));
 	vbd.Usage = D3D11_USAGE_IMMUTABLE;
-	vbd.ByteWidth = sizeof vertices;
+	vbd.ByteWidth = sizeof vertices1;
 	vbd.BindFlags = D3D11_BIND_VERTEX_BUFFER;
 	vbd.CPUAccessFlags = 0;
 	// 新建顶点缓冲区
 	D3D11_SUBRESOURCE_DATA InitData;
 	ZeroMemory(&InitData, sizeof(InitData));
-	InitData.pSysMem = vertices;
+	InitData.pSysMem = vertices1;
 	HR(m_pd3dDevice->CreateBuffer(&vbd, &InitData, m_pVertexBuffer.GetAddressOf()));
+	//verteices2
+	D3D11_BUFFER_DESC vbd1;
+	ZeroMemory(&vbd1, sizeof(vbd1));
+	vbd1.Usage = D3D11_USAGE_IMMUTABLE;
+	vbd1.ByteWidth = sizeof vertices1;
+	vbd1.BindFlags = D3D11_BIND_VERTEX_BUFFER;
+	vbd1.CPUAccessFlags = 0;
+	D3D11_SUBRESOURCE_DATA InitData2;
+	ZeroMemory(&InitData2, sizeof(InitData2));
+	InitData2.pSysMem = vertices2;
+	HR(m_pd3dDevice->CreateBuffer(&vbd1, &InitData2, m_pVertexBuffer.GetAddressOf()));
+	//vertices3
+	D3D11_BUFFER_DESC vbd3;
+	ZeroMemory(&vbd3, sizeof(vbd3));
+	vbd3.Usage = D3D11_USAGE_IMMUTABLE;
+	vbd3.ByteWidth = sizeof vertices1;
+	vbd3.BindFlags = D3D11_BIND_VERTEX_BUFFER;
+	vbd3.CPUAccessFlags = 0;
+	D3D11_SUBRESOURCE_DATA InitData3;
+	ZeroMemory(&InitData3, sizeof(InitData3));
+	InitData3.pSysMem = vertices3;
+	HR(m_pd3dDevice->CreateBuffer(&vbd3, &InitData3, m_pVertexBuffer.GetAddressOf()));
+	//vertices4
+	D3D11_BUFFER_DESC vbd4;
+	ZeroMemory(&vbd4, sizeof(vbd4));
+	vbd4.Usage = D3D11_USAGE_IMMUTABLE;
+	vbd4.ByteWidth = sizeof vertices1;
+	vbd4.BindFlags = D3D11_BIND_VERTEX_BUFFER;
+	vbd4.CPUAccessFlags = 0;
+	D3D11_SUBRESOURCE_DATA InitData4;
+	ZeroMemory(&InitData4, sizeof(InitData4));
+	InitData4.pSysMem = vertices4;
+	HR(m_pd3dDevice->CreateBuffer(&vbd4, &InitData4, m_pVertexBuffer.GetAddressOf()));
 
 
 	// ******************
